3540f3765dd25f52f6b79a2a7ff9aad3f608ae1e
limzu <p.limzu12@gmail.com>
2022-09-08 18:43:49 +0900

    " Second Commit"

--- src/chpter07_ex03/Using_Constructor.java
+++ src/chpter07_ex03/Using_Constructor.java
@@ -1,10 +1,77 @@
 package chpter07_ex03;
 
+class AA {
+	
+	//필드
+	int a;
+	int b;
+	int c;
+	int d;
+	Stirng name ;
+	
+	//생성자
+	//1. 기본생성자
+	AA () { int a{
+		this.a=a;
+		
+	}
+	
+	AA ( int a, int b) {
+		this.a=a;
+		this.b=b;
+		
+		
+	}
+	AA (int a, int b, int c) {
+		this.a=a;
+		this.b=b;
+		this.c=c;
+		
+	}
+	AA (int a, intb, int c, int d, String e) {
+		this.a=a;
+		this.b=b;
+		this.c=c;
+		this.d=d;
+	AA
+		System.out.println("기본생성자 호출");
+		
+	}
+	void show() {System.out.println(;"a : " + "b : " + "c:" + c + "d: " + d);
+		System.out.println("name:" + name);
+
+	
+	}
+}
+
+
 public class Using_Constructor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Constructor 생성자 오버로딩
+		
+		//객체 생성 (기본생성자 호출)
+		AA aa1 = new AA(); //기본생성자 호출
+		aa1.show();
+		System.out.println("==============");
+		//매개변수 1개인 기본생성자 호출
+		AA aa2 = new AA(10);
+		aa2.show();
+		System.out.println("===================");
+		
+		//매개변수 2개인 생성자호출
+		AA aa3 = new AA (10, 20);
+		aa3.show();
+		
+		//매개변수 3개인 생성자호출.
+		AA aa4 new AA (10,20,30);
+		aa4.show();
+		System.out.println("================");
+		AA aa5 = new AA (10,20,30,40)
+		aa5.show();
+		System.out.println("================");
+		AA aa6 = new AA (10,20,30,40,aa1 "홍길동");
+		aa6.show();
 	}
 
 }
--- src/chpter07_ex03/Using_Constructor03.java
+++ src/chpter07_ex03/Using_Constructor03.java
@@ -1,9 +1,84 @@
 package chpter07_ex03;
 
+class BB {
+	//필드
+	int kor;
+	int eng;
+	int mat;
+	String name;
+	
+	BB() {} //기본생성자
+	BB( int a, int b, int c, String name) {	//객체생성시 필드의 값을 초기화
+		kor = a;
+		eng = b;
+		mat = c;
+		this.name= name;
+	}
+	
+	//getter, setter를 사용한 필드의 값을 초기화 <========필드의 초기값 할당.
+	// Setter : 필드의 초기값을 할당.
+	// Getter : 필드의 값을 출력할때 사용.
+	
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMat() {
+		return mat;
+	}
+	public void setMat(int mat) {
+		this.mat = mat;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	void show () {
+		System.out.println(kor + " , " + eng + " , " + mat + " , " + name);
+	}
+	//getter, setter를 사용한 필드의 값을 초기화합니다.
+	
+	
+	
+}
+
 public class Using_Constructor03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//필드의 값을 할당하는 3가지 방법.
+		
+		BB bb1 = new BB();
+		bb1.kor = 80;
+		bb1.mat = 70;
+		bb1.name = "홍길동"
+		bb1.show();
+		
+		//2. 생성자를 사용해서 필드의 초기값을 할당 
+		BB bb2 = new BB(70, 80, 90, "김유신");
+		bb.show();
+		
+		//3. Setter를 사용해서 값을 입력 <==
+		BB bb3 = new BB();
+		bb3.setEng(70);
+		bb3.setkor(80);
+		bb3.setMat(90);
+		bb3.setName("강감찬");
+		bb3.show();
+		
+		//1. 직접 필드의 값을 직접 할당하는 방법
+		
+		
 
 	}
 
